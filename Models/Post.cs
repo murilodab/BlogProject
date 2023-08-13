69a87c916fbf80a9e9e9e5dca59cf908156ce745
Murilo Barbosa <muriloab96@gmail.com>
2023-08-13 20:31:05 -0300

    Finished post.cs model

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlogProject.Models
 {
@@ -10,6 +11,32 @@ namespace BlogProject.Models
         public int BlogId { get; set; }
 
         public string AuthorId { get; set; }
+
+        [Required]
+        [StringLength(75, ErrorMessage = "The {0} muat be at least {2} and no more than {1} characters long", MinimumLength = 2)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(200, ErrorMessage = "The {0} muat be at least {2} and no more than {1} characters long", MinimumLength = 5)]
+        public string Abstract { get; set; }
+        [Required]
+        public string Content { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Updated Date")]
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+
+        public bool IsReady { get; set; }
+
+        public string Slug { get; set; }
+
+        public byte[] ImageData { get; set; }
+        public string ContentType { get; set; }
+
+        [NotMapped] //Not to be used by the Database
+        public IFormFile Image { get; set; }
+
 
     }
 }
