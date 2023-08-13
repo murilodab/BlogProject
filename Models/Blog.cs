4f1d1302fadb8ae85febfb99c6480b42167d3e48
Murilo Barbosa <muriloab96@gmail.com>
2023-08-12 22:46:18 -0300

    Created the Blog Model
    Created the Blog model

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -1,6 +1,35 @@
-﻿namespace BlogProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogProject.Models
 {
     public class Blog
     {
+        public int Id { get; set; }
+        public string AuthorId { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 5)]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Created Date")]
+        public DateTime Created { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Updated Date")]
+        public DateTime? Updated { get; set; }
+
+        [Display(Name = "Blog Image")]
+        public byte[] ImageData { get; set; }
+
+        public string ContentType { get; set; }
+
+        [NotMapped]
+        public IFormFile Image { get; set; }
     }
 }
