352f70b011cedebb7adb2fa6d6fc32a80de76080
Murilo Barbosa <muriloab96@gmail.com>
2023-08-12 22:47:27 -0300

    Created the Post Model
    Post Model with a few attributes, still missing some.

---
+++ Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogProject.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Blog Name")]
+        public int BlogId { get; set; }
+
+        public string AuthorId { get; set; }
+
+    }
+}
