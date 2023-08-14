54647bffa0b9bd42b2b9a4b6cbdb8b24b3d41c79
Murilo Barbosa <muriloab96@gmail.com>
2023-08-13 21:29:48 -0300

    Updated Comment.cs

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -1,6 +1,35 @@
-﻿namespace BlogProject.Models
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogProject.Models
 {
     public class Comment
     {
+        public int Id { get; set; }
+        public string PostId { get; set; }
+        public string AuthorId { get; set; }
+        public string ModeratorId { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2}, and no more than {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "Comment")]
+        public string Body { get; set; }
+
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+        public DateTime? Moderated { get; set; }
+        public DateTime? Deleted { get; set; }
+
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2}, and no more than {1} characters long.", MinimumLength = 2)]
+        [Display(Name = "Moderated Comment")]
+        public string ModeratedBody { get; set; }
+
+        //Navigation properties (Relationships)
+        public virtual Post Post { get; set; }
+        public virtual IdentityUser Author { get; set; }
+        public virtual IdentityUser Moderator { get; set; }
+
+
+
     }
 }
---
+++ Models/Tag.cs
@@ -0,0 +1,6 @@
+﻿namespace BlogProject.Models
+{
+    public class Tag
+    {
+    }
+}
