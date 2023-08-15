e4ca001367bc78d14d8d2b59c6acd3185c23c1a8
Murilo Barbosa <muriloab96@gmail.com>
2023-08-15 10:55:00 -0300

    Added enum to manage moderation types

---
+++ Enums/ModerationType.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace BlogProject.Enums
+{
+    public enum ModerationType
+    {
+        [Description("Political Propaganda")]
+        Political,
+        [Description("Offensive Language")]
+        Language,
+        [Description("Drug References")]
+        Drugs,
+        [Description("Threatening Speech")]
+        Threatening,
+        [Description("Sexual Content")]
+        Sexual,
+        [Description("Hate Speech")]
+        HateSpeech,
+        [Description("Target Shaming")]
+        Shaming
+    }
+}
--- Models/Comment.cs
+++ Models/Comment.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using BlogProject.Enums;
+using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 
 namespace BlogProject.Models
@@ -23,6 +24,10 @@ namespace BlogProject.Models
         [StringLength(500, ErrorMessage = "The {0} must be at least {2}, and no more than {1} characters long.", MinimumLength = 2)]
         [Display(Name = "Moderated Comment")]
         public string ModeratedBody { get; set; }
+
+        public ModerationType ModerationType { get; set; }
+
+
 
         //Navigation properties (Relationships)
         public virtual Post Post { get; set; }
