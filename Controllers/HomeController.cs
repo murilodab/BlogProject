cfa9556a9c4c31022ce53c8104a5aa93ddfe1ab5
Murilo Barbosa <muriloab96@gmail.com>
2023-09-05 18:08:46 -0300

    fixed navigation links for initial carving

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,7 +18,12 @@ namespace BlogProject.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+       public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
         {
             return View();
         }
