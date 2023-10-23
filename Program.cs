cdca08493125f0609b415a771ec6c2308b3abfc3
Murilo Barbosa <muriloab96@gmail.com>
2023-10-23 14:21:23 -0300

    update to program.cs

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ using static System.Formats.Asn1.AsnWriter;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = ConnectionHelper.GetConnectionString(builder.Configuration) ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
 //builder.Services.AddDbContext<ApplicationDbContext>(options =>
 //  options.UseSqlServer(connectionString));
