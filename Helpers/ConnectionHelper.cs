265e11c262def5fb091d784a9837027bb58f0c1a
Murilo Barbosa <muriloab96@gmail.com>
2023-10-23 14:30:12 -0300

    altered conneciton helper

--- Helpers/ConnectionHelper.cs
+++ Helpers/ConnectionHelper.cs
@@ -6,7 +6,7 @@ namespace BlogProject.Helpers
     {
         public static string GetConnectionString(IConfiguration configuration)
         {
-            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var connectionString = configuration.GetSection("ConnectionStrings")["DefaultConnection"];
             var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
             return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
         }
